e4b20bbea08b51f2aab95150898fb5ab541a3ba9
Raphaela Castro <raphaelaoliveira79@gmail.com>
2020-10-19 21:57:14 -0300

    Novas atividades

--- app/src/main/java/br/usjt/fire/MainActivity.java
+++ app/src/main/java/br/usjt/fire/MainActivity.java
@@ -5,20 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText loginEditText;
+    private EditText senhaEditText;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginEditText = findViewById(R.id.loginEditText);
+        senhaEditText = findViewById(R.id.senhaEditText);
+        auth = FirebaseAuth.getInstance();
     }
 
-    public void irParaCastro(View view) {
+    public void irParaCadastro (View view){
         startActivity(new Intent(this, NovoUsuarioActivity.class));
     }
 
-    public void fazerLogin(View v) {
-
+    public void fazerLogin (View v){
+        String login = loginEditText.getEditableText().toString();
+        String senha = senhaEditText.getEditableText().toString();
+        auth.signInWithEmailAndPassword(login, senha).
+                addOnSuccessListener((result) -> {
+                    Toast.makeText(this, "sim, logou", Toast.LENGTH_SHORT).show();
+                }).
+                addOnFailureListener((error) -> {
+                    Toast.makeText(this, "não, não logou", Toast.LENGTH_SHORT).show();
+                });
     }
 }
--- app/src/main/java/br/usjt/fire/NovoUsuarioActivity.java
+++ app/src/main/java/br/usjt/fire/NovoUsuarioActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,6 +25,10 @@ public class NovoUsuarioActivity extends Activity {
     public void criarNovoUsuario (View view) {
         String login = loginNovoUsuarioEditText.getEditableText().toString();
         String senha = senhaNovoUsuarioEditText.getEditableText().toString();
-        auth.createUserWithEmailAndPassword(login, senha).addOnSuccessListener();
+        auth.createUserWithEmailAndPassword(login, senha).addOnSuccessListener((result) ->{
+            Toast.makeText(this, result.getUser().getEmail(), Toast.LENGTH_SHORT).show();
+            finish();
+
+        }).addOnFailureListener((error -> error.printStackTrace()));
     }
 }
